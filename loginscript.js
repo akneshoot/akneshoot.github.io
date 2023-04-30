d0bcee34c9d3e20de303ce32d5668ca2dc9e6c2b
akneshoot <nastyasid04@gmail.com>
2023-04-30 16:03:52 +0300

    commit

---
+++ loginscript.js
@@ -0,0 +1,17 @@
+let result;
+
+
+while(true)
+{
+result = prompt("Would you like to register on the site?");
+
+    if (result == "Yes")
+{
+alert("Cool!");
+        break;
+    }
+    else
+    {
+alert("Try again");
+    }
+};
